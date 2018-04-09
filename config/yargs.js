24d185c938029ddf7b60f251ab5aafcaa11b3b42
eduardogm81qg <QuarkGluon123>
2018-04-09 11:01:22 +0200

    Se ha optimizado un poco el código de configuración de yargs.

--- config/yargs.js
+++ config/yargs.js
@@ -6,32 +6,28 @@
  *         --completado / -c --> default: true
  * --help
  */
+const descripcion = {
+    demand: true,
+    alias: 'd',
+    desc: 'Descripcion de la tarea por hacer'
+};
+const completado = {
+    alias: 'c',
+    default: true,
+    desc: 'Marca como completada o pendiente la tarea'
+};
+
+
 const argv = require('yargs')
     .command('crear', 'Crear un elmento por hacer', {
-        descripcion: {
-            demand: true,
-            alias: 'd',
-            desc: 'Descripcion de la tarea por hacer'
-        }
+        descripcion
     })
     .command('actualizar', 'Actualiza el estado completado de una tarea', {
-        descripcion: {
-            demand: true,
-            alias: 'd',
-            desc: 'Descripcion de la tarea por hacer'
-        },
-        completado: {
-            alias: 'c',
-            default: true,
-            desc: 'Marca como completada o pendiente la tarea'
-        }
+        descripcion,
+        completado
     })
     .command('borrar', 'Borrar una tarea', {
-        descripcion: {
-            demand: true,
-            alias: 'd',
-            desc: 'Descripcion de la tarea por hacer'
-        }
+        descripcion
     })
     .help()
     .argv;
